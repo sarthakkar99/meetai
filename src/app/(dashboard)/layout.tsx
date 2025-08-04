068f10d77d129ad1070d22997a63398ff6964c0a
sarthakkar99 <sarthak.kar2016@gmail.com>
2025-08-04 15:15:38 -0700

    07:  dashboard navbar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,4 +1,5 @@
 import { SidebarProvider } from '@/components/ui/sidebar';
+import { DashboardNavbar } from '@/modules/dashboard/ui/components/dashboard-navbar';
 import { DashboardSidebar } from '@/modules/dashboard/ui/components/dashboard-sidebar';
 import React from 'react'
 interface Props{
@@ -8,7 +9,10 @@ const Layout = ({children}: Props) => {
   return (
      <SidebarProvider>
         <DashboardSidebar/>
+        <main className='flex flex-col h-screen w-screen bg-muted'>
+          <DashboardNavbar/>
             {children}
+        </main>
        </SidebarProvider>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
   subsets: ["latin"],
 });
 
@@ -25,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.className} antialiased`}
       >
         {children}
       </body>
