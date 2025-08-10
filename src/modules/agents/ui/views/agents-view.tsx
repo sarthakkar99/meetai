ee7a80e8e659976dbea79ef0775a0d5d30cc08c0
sarthakkar99 <sarthak.kar2016@gmail.com>
2025-08-10 16:39:29 -0700

    10: Reponsive Dialog

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -5,12 +5,14 @@ import { ErrorState } from "@/components/error-state";
 
 import { LoadingState } from "@/components/loading-state";
 
+
 export const AgentsView = () => {
     const trpc = useTRPC();
     const {data} = useSuspenseQuery(trpc.agents.getMany.queryOptions());
     
     return (
         <div>
+            
             {JSON.stringify(data, null, 2)}
         </div>
     );
